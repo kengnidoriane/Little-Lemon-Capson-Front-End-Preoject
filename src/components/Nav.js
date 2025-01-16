e4a92ced56debd055188009befbc7dea5e6928b0
kengnidoriane <dorianekengni@gmail.com>
2025-01-16 12:04:56 +0100

    setting up semantic tags on new components files

---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <>
+        <img alt='logo'/>
+        <ul>
+            <li>
+                <a href='#home'>Home</a>
+            </li>
+            <li>
+                <a href='#about'>About</a>
+            </li>
+            <li>
+                <a href='#reservation'>Reservation</a>
+            </li>
+            <li>
+                <a href='#orderOnline'>Order Online</a>
+            </li>
+            <li>
+                <a href='.\login'>Login</a>
+            </li>
+        </ul>
+    </>
+  )
+}
