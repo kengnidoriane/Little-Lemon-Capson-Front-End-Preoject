11132eb5a52d6ec177b22795295591a40a054230
unknown <dorianekengni@gmail.com>
2025-03-12 17:22:00 +0200

    add About and Specials components sections

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './About.css'; // Assurez-vous d'importer le fichier CSS pour le style
+
+const About = () => {
+    return (
+        <section className="about">
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+                Bienvenue chez Little Lemon, où nous servons des plats délicieux inspirés de la cuisine méditerranéenne. 
+                Notre mission est de vous offrir une expérience culinaire exceptionnelle avec des ingrédients frais et de qualité.
+            </p>
+            <div className="photo-container">
+                <img src="/images/photo1.jpg" alt="Photo 1" className="owner restaurant" />
+                <img src="/images/photo2.jpg" alt="Photo 2" className="owner restaurant" />
+            </div>
+        </section>
+    );
+};
+
+export default About;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero';
+import SpecialMenu from '../components/SpecialMenu';
 
 const Homepage = () => {
   return (
     <>
         <Header />
         <Hero />
+        <SpecialMenu />
     </>
   )
 }
