40f2a3f6d38662b4bede11e2b0950d07f2620ac9
unknown <dorianekengni@gmail.com>
2025-03-12 17:48:14 +0200

    fix: import component in Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero';
 import SpecialMenu from '../components/SpecialMenu';
+import Testimonial from '../components/Testimonial';
+import About from '../components/About';
+import Footer from '../components/Footer';
 
 const Homepage = () => {
   return (
@@ -9,6 +12,9 @@ const Homepage = () => {
         <Header />
         <Hero />
         <SpecialMenu />
+        <Testimonial />
+        <About />
+        <Footer />
     </>
   )
 }
